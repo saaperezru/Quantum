547ee3727468ae1b811a8242a7b852c653ab5036
Santiago Pérez <santiago1991@gmail.com>
2011-12-11 22:58:59 -0500

    Evolution of model with construction of basis and reconstruction

--- Modelo.py
+++ Modelo.py
@@ -6,13 +6,25 @@ class Basis:
   def addFeature(self,weight,feature):
     self.feature.append((weight,feature))
 
+  def orderFeatures(self,method):
+    self.feature = method(self.feature)
+
+
 class Object:
   def __init__(self,name,path):
     self.name = name
     self.path = path
     self.rep = []
+    self.reconstruction = []
   def addRepresentation(self,weight,basis):
+    """Adds a pair weight, basis to the representation of the object"""
     self.rep.append((weight,basis))
+  def addReconstructionFeature(self,weight,feature):
+    """Adds a pair weight, feature to the new reconstructed representation of the object"""
+    self.reconstruction.append((weight,feature))
+  def orderBasis(self,method):
+    """Order the elements of the basis of the new space according to their weights using mehtod"""
+    self.rep = method(self.rep)
 
 class Feature:
   
@@ -39,11 +51,15 @@ class Reduction:
       for i in range(B.shape[0]):
         tmp.addFeature(B[i,j],features[i])
       self.basis.append(tmp)
+    self.basis.orderFeatures(self.orderFeaturesInBasis)
     for j in range(R.shape[1]):
       for i in range(R.shape[0]):
         documents[j].addRepresentation(R[i,j],self.basis[i])
-
-
+      for i in range(self.reconstructed.shape[0]):
+        documents[j].addReconstructionFeauter(self.reconstructed[i,j],features[i])
+      documents[j].orderBasis(self.orderBasisInObject)
+  def getBasis(self):
+    return orderBasisInObject
   def addBasis(self,basis):
     self.basis.append(basis)
   def addObject(self,obj):
