1f1f4b54b5efab866d042e17a8d0577c569b9b7a
Santiago en CUDA <santiago1991@gmail.com>
2012-03-23 09:54:48 -0500

    Added experiment for squaring the NMF esults so to see if results get to compite with QLSA squared basis converted in interference and basis

--- matlab/test_complex_back.m
+++ matlab/test_complex_back.m
@@ -3,18 +3,38 @@ dataset =  '/datasets/complex_back/'
 data = '../../../datasets/complex_back/';
 pattern = 'airport*.*';
 
-dataPath = '../../experiments/complex_back_airport15/';
-basisPath = '../../experiments/complex_back_airport15/basis/';
-LPath = '../../experiments/complex_back_airport15/l/';
-IPath = '../../experiments/complex_back_airport15/i/';
-recPath = '../../experiments/complex_back_airport15/rec/';
+dataPath = '../../experiments/complex_back_airport15/QLSA';
+
+basisPath = '../../experiments/complex_back_airport15/QLSA/basis/';
+LPath = '../../experiments/complex_back_airport15/QLSA/l/';
+IPath = '../../experiments/complex_back_airport15/QLSA/i/';
+recPath = '../../experiments/complex_back_airport15/QLSA/rec/';
 
 folderroot='/home/saaperezru/QLSA/scripts';
 addpath(fullfile(folderroot,dataset));
 
 [X dim] = loadMatrix(data,pattern);
 r = 15;
+
+
+
+
 [B,R,Xh,L,S] = QLSA(X,r,dataPath);
+clear R;
+viewer(B,basisPath,'b',dim);
+viewer(L,LPath,'L',dim);
+viewer(S,IPath,'I',dim);
+viewer(Xh,recPath,'img',dim);
+
+
+dataPath = '../../experiments/complex_back_airport15/NMF';
+basisPath = '../../experiments/complex_back_airport15/NMF/basis/';
+LPath = '../../experiments/complex_back_airport15/NMF/l/';
+IPath = '../../experiments/complex_back_airport15/NMF/i/';
+recPath = '../../experiments/complex_back_airport15/NMF/rec/';
+
+X = X+0.001;
+[B,R,Xh,L,S] = NMF(X,r,dataPath);
 clear R;
 viewer(B,basisPath,'b',dim);
 viewer(L,LPath,'L',dim);
